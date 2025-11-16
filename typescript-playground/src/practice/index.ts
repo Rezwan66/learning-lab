2bc403ec732ea5940e4a0aba9833cbce84df19a5
Rezwan66 <shaikhrezwan66@gmail.com>
2025-11-16 15:13:06 +0100

    add new problem solve

--- typescript-playground/src/practice/index.ts
+++ typescript-playground/src/practice/index.ts
@@ -60,3 +60,74 @@ class Car extends Vehicle {
 const myCar = new Car('Toyota', 2020, 'Corolla');
 // console.log(myCar.getInfo());
 // console.log(myCar.getModel());
+
+function processValue(value: string | number): number {
+  if (typeof value === 'string') {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+// console.log(processValue('hello'));
+// console.log(processValue(10));
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  //   if (products.length === 0) {
+  //     return null;
+  //   }
+
+  const mostExpensiveProduct = products.sort((a, b) => b.price - a.price);
+  return mostExpensiveProduct[0] ?? null;
+}
+const products = [
+  { name: 'Pen', price: 10 },
+  { name: 'Notebook', price: 25 },
+  { name: 'Bag', price: 50 },
+];
+
+// console.log(getMostExpensiveProduct(products));
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return 'Weekend';
+  } else {
+    return 'Weekday';
+  }
+}
+// console.log(getDayType(Day.Monday));
+// console.log(getDayType(Day.Tuesday));
+// console.log(getDayType(Day.Wednesday));
+// console.log(getDayType(Day.Thursday));
+// console.log(getDayType(Day.Friday));
+// console.log(getDayType(Day.Saturday));
+// console.log(getDayType(Day.Sunday));
+
+async function squareAsync(n: number): Promise<number> {
+  const squarePromise = new Promise<number>((resolve, reject) => {
+    if (n >= 0) {
+      setTimeout(() => {
+        resolve(n * n);
+      }, 1000);
+    } else {
+      reject('Negative number not allowed');
+    }
+  });
+  return squarePromise;
+}
+squareAsync(4).then(console.log);
+// squareAsync(-3).catch(console.error);
