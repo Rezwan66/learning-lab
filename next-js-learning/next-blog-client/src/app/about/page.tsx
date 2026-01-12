670cdc336c4c1c7a2346fa19eddab89280b0a436
Rezwan66 <shaikhrezwan66@gmail.com>
2026-01-12 22:52:09 +0100

    init nextjs blog app with shadcn

---
+++ next-js-learning/next-blog-client/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h2>AboutPage page</h2>
+    </div>
+  );
+}
---
+++ next-js-learning/next-blog-client/src/app/contact/branch/page.tsx
@@ -0,0 +1,7 @@
+export default function BranchPage() {
+  return (
+    <div>
+      <h2>BranchPage nested page</h2>
+    </div>
+  );
+}
