8722566224291e2a80d4e545cfb78ba8784b7201
Rezwan66 <shaikhrezwan66@gmail.com>
2026-01-13 02:16:31 +0100

    added navbar comp

---
+++ next-js-learning/next-blog-client/src/app/blogs/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogsPage() {
+  return (
+    <div>
+      <h2>BlogsPage page</h2>
+    </div>
+  );
+}
---
+++ next-js-learning/next-blog-client/src/app/contact/layout.tsx
@@ -0,0 +1,12 @@
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h2>ContactLayout page</h2>
+      {children}
+    </div>
+  );
+}
