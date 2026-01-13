af3934518bf57c3a4ccec84b949d98a37a570dbf
Rezwan66 <shaikhrezwan66@gmail.com>
2026-01-13 02:22:52 +0100

    test commit

--- next-js-learning/next-blog-client/src/app/page.tsx
+++ next-js-learning/next-blog-client/src/app/page.tsx
@@ -3,8 +3,9 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className="">
+      HomePage
       <Button className="cursor-pointer" variant="outline">
-        Click Here
+        Click here
       </Button>
     </div>
   );
